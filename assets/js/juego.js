142caec5d070f5238de0e02088cd2c9eea099335
Cesar-Dev <cesarvargasescorcia@gmail.com>
2021-02-02 17:03:09 -0600

    Creando los arreglos para dar forma al deck de cartas

---
+++ assets/js/juego.js
@@ -0,0 +1,34 @@
+/*-- 
+*"2C == Dos de treboles" 
+*"2D == Dos de diamantes" 
+*"2H == Dos de corazones" 
+*"2S == Dos de espadas" 
+                            --*/
+
+let deck         = [];
+const tipos      = ['C', 'D', 'H', 'S'];
+const especiales = ['A', 'J', 'Q', 'K'];
+
+
+// for (let i = 0; i <= tipos.length; i++) {
+//     console.log(tipos[i]);
+// }
+
+const crearDeck = () => {
+    for (let i = 2; i <= 10; i++) {
+       for (const tipo of tipos) {
+           deck.push(i + tipo);
+       }
+    }
+
+    for (const tipo of tipos) {
+        for (const especial of especiales) {
+            deck.push(especial + tipo);
+        }
+    }
+
+    console.log(deck);
+}
+
+
+crearDeck();
