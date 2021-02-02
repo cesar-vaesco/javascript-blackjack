26499283cb57d138828a005b95d20dd6e5221f56
Cesar-Dev <cesarvargasescorcia@gmail.com>
2021-02-02 17:31:24 -0600

    Configuración para pedir una carta del deck[1;5D

--- assets/js/juego.js
+++ assets/js/juego.js
@@ -5,20 +5,20 @@
 *"2S == Dos de espadas" 
                             --*/
 
-let deck         = [];
-const tipos      = ['C', 'D', 'H', 'S'];
+let deck = [];
+const tipos = ['C', 'D', 'H', 'S'];
 const especiales = ['A', 'J', 'Q', 'K'];
 
 
 // for (let i = 0; i <= tipos.length; i++) {
 //     console.log(tipos[i]);
 // }
-
+// Esta función crea una nueva baraja -deck
 const crearDeck = () => {
     for (let i = 2; i <= 10; i++) {
-       for (const tipo of tipos) {
-           deck.push(i + tipo);
-       }
+        for (const tipo of tipos) {
+            deck.push(i + tipo);
+        }
     }
 
     for (const tipo of tipos) {
@@ -27,14 +27,34 @@ const crearDeck = () => {
         }
     }
 
-    console.log(deck);
-
+    //console.log(deck);
     deck = _.shuffle(deck);
-
-    console.log( deck );
+    console.log(deck);
 
     return deck;
 }
 
 
 crearDeck();
+
+
+// Esta función permite tomar una carta
+
+const pedirCarta = () => {
+
+    // Validar existencia de cartas
+
+    if (deck.length === 0) {
+        throw 'No hay cartas en el deck';
+    }
+
+    let carta = deck.pop();
+
+    console.log(deck);
+    console.log({ carta });
+    // Carta debe de ser de la baraja
+    return carta;
+}
+
+
+// pedirCarta();
